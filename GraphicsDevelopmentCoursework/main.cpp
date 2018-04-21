f52d6a6051a655323053ae5a7a564dc4ca19f436
Antoine <antoine.sb@orange.fr>
2018-04-21 12:19:14 +0100

    Adding SDL2

--- GraphicsDevelopmentCoursework/main.cpp
+++ GraphicsDevelopmentCoursework/main.cpp
@@ -1,5 +1,9 @@
 #include "main.hpp"
 
 int main(int argc, char* argv[]) {
+	for (unsigned int i = 0; i < (unsigned int)argc; i++)
+		printf("Argument %ld : %s \n", i + 1, argv[i]);
+
+	getchar();
 	return 0;
 }
