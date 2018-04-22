6b6b87c14cbd9b3751edbe16bc8603f29442deec
Antoine <antoine.sb@orange.fr>
2018-04-22 11:33:06 +0100

    Changing files encoding + extracting control blocks into functions + translations

--- GraphicsDevelopmentCoursework/main.cpp
+++ GraphicsDevelopmentCoursework/main.cpp
@@ -1,69 +1,100 @@
-#include "main.hpp"
+﻿#include "main.hpp"
 
 using namespace std;
+
+void setOpenGLAttributes() {
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+}
+
+bool SDLInitialization() {
+	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+		cout << "Error during the initialization of the SDL : " << SDL_GetError() << endl;
+		SDL_Quit();
+		return false;
+	}
+	return true;
+}
+
+bool windowCreation(SDL_Window* window) {
+	if(window == nullptr) {
+		cout << "Error during the window creation : " << SDL_GetError() << endl;
+		SDL_Quit();
+		return false;
+	}
+	return true;
+}
+
+bool contextCreation(SDL_GLContext OpenGLContext, SDL_Window* window) {
+	if(OpenGLContext == nullptr) {
+		cout << "Error during the context creation : " << SDL_GetError() << endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return false;
+	}
+	return true;
+}
+
+bool GLEWInitialization(GLenum initialisationGLEW, SDL_GLContext OpenGLContext, SDL_Window* window) {
+	if(initialisationGLEW != GLEW_OK) {
+		cout << "Error during the initialization of GLEW : " << glewGetErrorString(initialisationGLEW) << endl;
+		SDL_GL_DeleteContext(OpenGLContext);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return -1;
+	}
+	return true;
+}
 
 int main(int argc, char* argv[]) {
 	for(unsigned int i = 0; i < (unsigned int)argc; i++)
 		printf("Argument %ld : %s \n", i + 1, argv[i]);
 
-	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
-		cout << "Erreur lors de l'initialisation de la SDL : " << SDL_GetError() << endl;
-		SDL_Quit();
+	if(!SDLInitialization())
 		return -1;
-	}
 
-	// Version d'OpenGL
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-	// Double Buffer
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	setOpenGLAttributes();
 
-	SDL_Window* fenetre(0);
-	fenetre = SDL_CreateWindow("Phusis krupthestai philei", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1600, 900, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
-	if(fenetre == 0) {
-		cout << "Erreur lors de la creation de la fenetre : " << SDL_GetError() << endl;
-		SDL_Quit();
+	SDL_Window* window = SDL_CreateWindow("Anthony Sébert 1705851", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+	if(!windowCreation(window))
 		return -1;
-	}
 
-	SDL_GLContext contexteOpenGL(0);
-	contexteOpenGL = SDL_GL_CreateContext(fenetre);
-	if(contexteOpenGL == 0) {
-		cout << SDL_GetError() << endl;
-		SDL_DestroyWindow(fenetre);
-		SDL_Quit();
+	SDL_GLContext OpenGLContext = SDL_GL_CreateContext(window);
+	if(!contextCreation(OpenGLContext, window))
 		return -1;
-	}
 
 	GLenum initialisationGLEW(glewInit());
-	if(initialisationGLEW != GLEW_OK) {
-		cout << "Erreur d'initialisation de GLEW : " << glewGetErrorString(initialisationGLEW) << endl;
-		SDL_GL_DeleteContext(contexteOpenGL);
-		SDL_DestroyWindow(fenetre);
-		SDL_Quit();
+	if(!GLEWInitialization(initialisationGLEW, OpenGLContext, window))
 		return -1;
-	}
 
-	float vertices[] = { -0.5, -0.5,   0.0, 0.5,   0.5, -0.5 };
-	SDL_Event evenements;
-	bool terminer(false);
+	float vertices[] = {
+		-0.5,
+		-0.5,
+		 0.0,
+		 0.5,
+		 0.5,
+		-0.5
+	};
+	SDL_Event events;
+	bool terminate(false);
 
-	while(!terminer) {
-		SDL_WaitEvent(&evenements);
-		if(evenements.window.event == SDL_WINDOWEVENT_CLOSE)
-			terminer = true;
+	while(!terminate) {
+		SDL_WaitEvent(&events);
+		if(events.window.event == SDL_WINDOWEVENT_CLOSE)
+			terminate = true;
 		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, vertices);
 		glEnableVertexAttribArray(0);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glDisableVertexAttribArray(0);
-		SDL_GL_SwapWindow(fenetre);
+		SDL_GL_SwapWindow(window);
 	}
 
 	getchar();
 
-	SDL_GL_DeleteContext(contexteOpenGL);
-	SDL_DestroyWindow(fenetre);
+	SDL_GL_DeleteContext(OpenGLContext);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
--- GraphicsDevelopmentCoursework/main.hpp
+++ GraphicsDevelopmentCoursework/main.hpp
@@ -1,6 +1,9 @@
-#pragma once
+﻿#pragma once
 
 #include <SDL.h>
 #include <stdio.h>
 #include <iostream>
 #include <GL/glew.h>
+#include <utility>
+
+typedef std::pair<double, double> point;
